58b4eb5cdf8b623d875e8f70edee2555a1aaf69b
vladimirdubovsky <vladimir.dubovsky@outlook.com>
2023-12-13 21:53:03 +0100

    hello everything

---
+++ src/main/java/io/vdubovsky/everythingpublic/EverythingPublicApplication.java
@@ -0,0 +1,13 @@
+package io.vdubovsky.everythingpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverythingPublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vdubovsky/everythingpublic/EverythingPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vdubovsky.everythingpublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
