046e32d293b36367699d1f028db481e81baa53cf
vladimirdubovsky <vladimir.dubovsky@outlook.com>
2023-12-17 19:54:56 +0100

    Sting and GString

---
+++ groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/string/StringGStringRunner.groovy
@@ -0,0 +1,19 @@
+package io.vdubovsky.groovytraining.string
+
+class StringGStringRunner {
+
+     static void main(String[] args) {
+        def string1 = "hello 'Vova'"
+        def string2 = 'hello "Vova"'
+        def string3 = 'hello \'Vova\''
+        def string4 = "hello \"Vova\""
+
+        println string1[0]
+        println string1[-1]
+        println string1[1..3]
+
+        println((string2 * 3) - 'll')
+
+         // see Hello World app
+    }
+}
