e64db7c5b07e276109c0165fd72e0a29e5beb2ec
vladimirdubovsky <vladimir.dubovsky@outlook.com>
2023-12-17 18:48:34 +0100

    Operators

---
+++ groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/operator/OperatorJavaRunner.java
@@ -0,0 +1,14 @@
+package io.vdubovsky.groovytraining.operator;
+
+public class OperatorJavaRunner {
+
+    public static void main(String[] args) {
+        Integer a = 130;
+        Integer b = 65 * 2;
+        System.out.println(a == b);
+
+        Integer a1 = 110;
+        Integer b1 = 55 * 2;
+        System.out.println(a1 == b1);
+    }
+}
---
+++ groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/operator/OperatorRunner.groovy
@@ -0,0 +1,22 @@
+package io.vdubovsky.groovytraining.operator
+
+class OperatorRunner {
+
+    static void main(String[] args) {
+        assert 5 / 2 == 2.5
+        assert 5.intdiv(2) == 2
+        assert 5 % 2 == 1
+
+        println 5 / 2 as Integer // def asType(clazz)
+
+        def a = 65
+        def b = 65
+        println ''
+        println 'assert integer'
+        println 130 === a + b
+        println 130 == a + b
+        println 130 <=> 130
+        println 130 <=> 131
+        println 130 <=> 129
+    }
+}
