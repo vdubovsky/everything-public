92e5a54c2f1591714da3fbd2d28529561b8f68ba
vladimirdubovsky <vladimir.dubovsky@outlook.com>
2023-12-25 08:51:13 +0100

    Switch runner

--- groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/condition/ConditionNullSafeRunner.groovy
+++ groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/condition/ConditionNullSafeRunner.groovy
@@ -6,9 +6,15 @@ class ConditionNullSafeRunner {
         def p = new Person()
         p = Math.random() > 0.5 ? p : null
         println p?.getX()
+        p?.doX()
     }
 
     static class Person {
         Integer x = 5
+
+        def doX() {
+            println "doX"
+            return x
+        }
     }
 }
---
+++ groovy-dmdev-training/src/main/groovy/io/vdubovsky/groovytraining/switcch/SwitchRunner.groovy
@@ -0,0 +1,49 @@
+package io.vdubovsky.groovytraining.switcch
+
+import groovy.transform.builder.Builder
+
+class SwitchRunner {
+
+    static void main(String[] args) {
+        def p = Math.random() > 0.5 ? null :
+                Person.builder()
+                        .name("Vova")
+                        .age(34)
+                        .build()
+
+        switch (p) {
+            case String:
+                println "String"
+                break
+            case Person.builder().build():
+                println "instance case"
+                break
+            case Person:
+                println "Person"
+                break
+            case [3, 5, 7, 9]:
+                println "in"
+                break
+            case { it?.getAge() === 34 }:
+                println "34"
+                break
+            case ~/\d+/:
+                println "regular"
+                break
+            default:
+                println "Default"
+                break
+        }
+    }
+
+    @Builder
+    static class Person {
+        def name
+        def age
+
+        def isCase(def switchValue) {
+            println 'is case ?'
+            false
+        }
+    }
+}
